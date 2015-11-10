20cd31e89b08ef7dece9c7e8cf5f7b27fc4dba8b
oscar <ujape@yahoo.es>
2015-11-10 20:13:38 +0100

    oscar commit

--- app/src/main/java/com/example/jordi/blablalanguage/LoginActivity.java
+++ app/src/main/java/com/example/jordi/blablalanguage/LoginActivity.java
@@ -3,21 +3,19 @@ package com.example.jordi.blablalanguage;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
-
 import android.content.CursorLoader;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
@@ -38,6 +36,7 @@ import static android.Manifest.permission.READ_CONTACTS;
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+/**/
 
     /**
      * Id to identity READ_CONTACTS permission request.
