41c68cf44b0bcd2bd77097e0d72b7bce3e13a020
oscar <ujape@yahoo.es>
2015-12-17 20:50:09 +0100

    Button of calling added

--- app/src/main/java/com/example/jordi/blablalanguage/Activitys/MeatingDetailActivity.java
+++ app/src/main/java/com/example/jordi/blablalanguage/Activitys/MeatingDetailActivity.java
@@ -1,11 +1,15 @@
 package com.example.jordi.blablalanguage.Activitys;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.jordi.blablalanguage.R;
 
@@ -18,6 +22,24 @@ public class MeatingDetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
+        final ImageButton button1 = (ImageButton) findViewById(R.id.imageButton);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                TextView text = (TextView) findViewById(R.id.telephoneNumber);
+                String tel = "tel:"+text.getText().toString();
+                Intent in=new Intent(Intent.ACTION_CALL,Uri.parse(tel));
+                try{
+                    startActivity(in);
+                }
+
+                catch (android.content.ActivityNotFoundException ex){
+
+                }
+            }
+        });
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
